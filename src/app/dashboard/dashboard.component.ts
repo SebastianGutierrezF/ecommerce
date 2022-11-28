6e99e1e3aa68b9a6c7fd646563fbafb67124d969
Maria Eugenia Gonzalez Rodriguez <l.gore020501@itses.edu.mx>
2022-11-28 08:42:38 -0600

    dasboard agregar Carrito

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  Productos: any = [] ;
+  ProductosxCategoria: any = [] ;
+  Categorias: any = [] ;
+  Comentarios: any = [] ;
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) {
+    this.ObtenerProductos();
+    this.ObtenerCategorias();
+    this.ObtenerComentarios();
+  }
 
   ngOnInit(): void {
+  }
+
+  ObtenerProductos() {
+    this.dataService.post('articulo', 'obtenerProductos', {}).subscribe((dato: any) => {
+      this.Productos = dato.reverse();
+    });
+  }
+
+  ObtenerProductosxCategoria(categoria: any) {
+    this.dataService.post('articulo', 'obtenerProductosxCategoria', {'idcat_a': categoria}).subscribe((dato: any) => {
+      this.ProductosxCategoria = dato.reverse();
+    });
+  }
+
+  ObtenerCategorias() {
+    this.dataService.post('categoria', 'traerCategorias', {}).subscribe((dato: any) => {
+      this.Categorias = dato;
+    });
+  }
+
+  ObtenerComentarios() {
+    this.dataService.post('comentario', 'obtenerComentarios', {}).subscribe((dato: any) => {
+      this.Comentarios = dato.reverse();
+    });
+  }
+
+  agregarAlCarrito(data: any) {
+
+    if(localStorage.getItem('carrito')){
+
+      var localComida = JSON.parse(localStorage.getItem('carrito')!);
+      localComida.push({ "id_a": data.id_a, "nombre_a": data.nombre_a, "precio": data.precio, "img_a": data.img_a});
+      
+      localStorage.setItem('carrito',JSON.stringify(localComida));
+
+    }else{
+
+      localStorage.setItem('carrito', JSON.stringify([{ "id_a": data.id_a, "nombre_a": data.nombre_a, "precio": data.precio, "img_a": data.img_a}]));
+
+    }
   }
 
 }
