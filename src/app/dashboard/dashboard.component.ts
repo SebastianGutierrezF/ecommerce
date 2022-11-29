2fcd7bc08abe671017ace0c66029bd953e31c54e
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-11-29 17:12:33 -0600

    Ver comentarios xnft y xcategoria

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,6 +15,7 @@ import { Oferta } from '../interfaces/oferta';
 export class DashboardComponent implements OnInit {
   selectedNft?: Articulo;
   comentarios?: any[];
+  comXCat?: any[] = [];
   comInput: FormGroup = this.fb.group({
     com: [, [Validators.required, Validators.maxLength(254)]],
     ida_com: [, Validators.required],
@@ -26,6 +27,8 @@ export class DashboardComponent implements OnInit {
   Categorias: any = [] ;
   categoria: any;
   productosTodos: any;
+  ofertas: string[] = [];
+  comCatSelected?: number;
 
   constructor(
     private ds: DataService,
@@ -48,8 +51,9 @@ export class DashboardComponent implements OnInit {
         data.forEach((oferta: any) => {
           if (new Date(oferta.fechain_o) <= hoy && new Date(oferta.fechafin_o) >= hoy) {
             desc += Number.parseInt(oferta.desc_o);
+            this.ofertas.push(oferta.banner_o);
           }
-        });
+        });        
         localStorage.setItem('desc_o', desc.toString());
       } else {
         alert("Ocurrio un error al intentar obtener ofertas");
@@ -73,25 +77,46 @@ export class DashboardComponent implements OnInit {
   }
 
   ObtenerCategorias() {
-    this.ds.post('categoria', 'traerCategorias', {}).subscribe((dato: any) => {
+    this.ds.get('categoria', 'traerCategorias').subscribe((dato: any) => {
       this.Categorias = dato;
     });
   }
 
   obtenerComentarios() {
     this.ds.get('comentario', 'obtenerComentarios').subscribe((dato: any) => {
-      this.comentarios = dato.reverse();
+      // this.comentarios = dato.reverse();
+      this.comXCat = dato.reverse();
     });
+  }
+
+  comentarioPorNft(id_a: number) {
+    this.ds.post('comentario', 'comentariosXNft', {id_a: id_a}).subscribe((dato: any) => {
+      if (dato) {
+        this.comentarios = dato.reverse();
+      }
+    });
+  }
+
+  comentariosPorCat(event: any) {
+    if (event.target.value == '0') {
+      this.obtenerComentarios();
+    } else {
+      this.ds.post('comentario', 'comentariosXCategoria', {id_c: event.target.value}).subscribe((data: any) => {
+        if (data) {
+          this.comXCat = data.reverse();
+        }
+      })
+    }
   }
 
   agregarAlCarrito(data: any) {
 
     if(localStorage.getItem('carrito')){
 
-      var localComida = JSON.parse(localStorage.getItem('carrito')!);
-      localComida.push({ "id_a": data.id_a, "nombre_a": data.nombre_a, "precio_a": data.precio_a, "img_a": data.img_a});
+      var localCarrito = JSON.parse(localStorage.getItem('carrito')!);
+      localCarrito.push({ "id_a": data.id_a, "nombre_a": data.nombre_a, "precio_a": data.precio_a, "img_a": data.img_a});
 
-      localStorage.setItem('carrito',JSON.stringify(localComida));
+      localStorage.setItem('carrito',JSON.stringify(localCarrito));
 
     }else{
 
@@ -102,27 +127,27 @@ export class DashboardComponent implements OnInit {
 
   abrirModal(nft: Articulo) {
     this.selectedNft = nft;
-    this.obtenerComentarios();
+    this.comentarioPorNft(this.selectedNft.id_a);
     this.comInput.patchValue({
       ida_com: this.selectedNft.id_a,
-      idu_com: '2'
+      // idu_com: localStorage.getItem('id_u')
+      idu_com: 1
     })
     this.modal = true;
-
   }
 
   cerrarModal() {
     this.selectedNft = undefined;
+    this.comentarios = [];
     this.modal = false;
   }
 
   insertarComentario() {
-    console.log(this.comInput.value);
-
     this.ds.post('comentario', 'insertarComentario', this.comInput.value).subscribe((data: any) => {
       if (data) {
+        this.comentarioPorNft(this.selectedNft!.id_a);
         this.obtenerComentarios();
-        this.comInput.reset();
+        this.comInput.controls['com'].reset();
       } else {
         alert("Ocurrio un error al agregar el comentario");
       }
