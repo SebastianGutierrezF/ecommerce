2356af63d01db1f024f922499d9db7f489b3fa23
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-11-30 19:07:38 -0600

    Traer ventas y sus respectivos articulos

--- src/app/ventas/ventas.component.ts
+++ src/app/ventas/ventas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../interfaces/articulo';
+import { Venta } from '../interfaces/venta';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-ventas',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ventas.component.css']
 })
 export class VentasComponent implements OnInit {
+  ventas: any[] = [];
+  articulos: any[] = [];
+  acordeon = false;
 
-  constructor() { }
+  constructor(private ds: DataService) {
+    this.ds.get('venta', 'traerVentas').subscribe((data: any) => {
+      if (data) {
+        this.ventas = data;
+      }
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+
+  buscarArticulos(id_v: number) {
+    this.ds.post('venta', 'traerArticulos', {id_v: id_v}).subscribe((data: any) => {
+      if (data) {
+        this.articulos = data;
+        this.acordeon = true;
+      }
+    })
   }
 
 }
